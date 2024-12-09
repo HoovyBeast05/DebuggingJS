97ca47bc053cb0bf981d561bf1bcba67cb13bfec
HoovyBeast05 <hussein05.ae@alumnes.ilerna.com>
2024-12-09 19:13:38 +0100

    Validación done

--- Validación Formulario/validacion_erronea.js
+++ Validación Formulario/validacion_erronea.js
@@ -1,7 +1,11 @@
-function validarFormulario(){
-    if(validarFormulario2()){
+function validarFormulario()
+{
+    if(validarFormulario2())
+    {
         alert("Formulario correcto!");
-    }else{
+    }
+    else
+    {
         alert("Formulario incorrecto!");
     }
 }
@@ -14,16 +18,16 @@ function validarFormulario2() {
     if (nombre.length < 3) {
         mensaje.innerText = "El nombre debe tener al menos 3 caracteres.";
         mensaje.style.color = "red";
-        return true; 
+        return false; 
     }
 
     if (!email.includes('@') || !email.includes('.')) {
         mensaje.innerText = "El email no es válido.";
         mensaje.style.color = "red";
-        return true; 
+        return false; 
     }
 
     mensaje.innerText = "Formulario enviado correctamente.";
     mensaje.style.color = "green";
-    return false;
+    return true;
 }
